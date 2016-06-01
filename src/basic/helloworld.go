1057f24f82abe5ba92d279bda21f7729fb9d613e
fqc <337940626@qq.com>
2016-06-01 13:36:11 +0800

    add go helloworld and _ not used

---
+++ src/basic/helloworld.go
@@ -0,0 +1,14 @@
+package main
+
+import(
+	"fmt" #notice _ not used
+	"runtime"
+)
+
+func main(){
+	
+	println("hello world!")
+	fmt.Printf("version:%s\n",runtime.Version())
+}
+
+
---
+++ src/basic/pkginit/logic/struct_decl.go
@@ -0,0 +1,16 @@
+package main
+
+tpye Person struct{
+	Name string
+	Age uint8
+	Address string
+}
+
+var p = Person
+
+
+
+
+
+
+
