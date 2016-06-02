41b0db41e5d2d5abf6fa37e15e328e8c1b2ec63a
fqc <337940626@qq.com>
2016-06-02 18:43:24 +0800

    add map declaration

---
+++ src/basic/map1/test_map.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+
+func main(){
+      m := map[string]int{"golang":1,"python":2,"java":3}//var m it's wrong
+
+     fmt.Printf("%d,%d,%d",m["golang"],m["python"],m["java"])
+}
