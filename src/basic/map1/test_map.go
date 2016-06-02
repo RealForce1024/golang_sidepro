0478e53916b33f0db58e9e337bd85e296f734fd8
fqc <337940626@qq.com>
2016-06-02 19:08:25 +0800

    delete(map,key)

--- src/basic/map1/test_map.go
+++ src/basic/map1/test_map.go
@@ -2,9 +2,8 @@ package main
 
 import "fmt"
 
-
-func main(){
-      m := map[string]int{"golang":1,"python":2,"java":3}//var m it's wrong
-
-     fmt.Printf("%d,%d,%d",m["golang"],m["python"],m["java"])
+func main() {
+	m := map[string]int{"golang": 1, "python": 2, "java": 3}
+	delete(m, "python")
+	fmt.Printf("%d,%d,%d", m["golang"], m["python"], m["java"], m["ruby"])
 }
